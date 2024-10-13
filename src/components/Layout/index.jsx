a6e5fd8d696cdcc64928d82f1f30f43786c7cb3f
Ernesto Leonard <ernestoleonard8@gmail.com>
2024-10-13 11:41:57 -0400

    El encabezado ahora redirecciona al home

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,15 +1,22 @@
 import LogoImage from "../LogoImage";
+import { NavLink } from "react-router-dom";
 
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <header className="bg-green-600 text-white py-6">
         <div className="container mx-auto px-4 flex justify-between items-center">
-          <div>
-            <h1 className="text-3xl font-bold">Blog de la Dieta Keto</h1>
-            <p className="mt-2">Tu guía para un estilo de vida más saludable</p>
-          </div>
-          <LogoImage />
+          <NavLink to="/">
+            <div>
+              <h1 className="text-3xl font-bold">Blog de la Dieta Keto</h1>
+              <p className="mt-2">
+                Tu guía para un estilo de vida más saludable
+              </p>
+            </div>
+          </NavLink>
+          <NavLink to="/">
+            <LogoImage />
+          </NavLink>
         </div>
       </header>
       {children}
